3e586c068a286f894ab70788c5c8fc83efe18c8b
Mark Deegan <mark.deegan@tudublin.ie>
2023-10-25 16:34:55 +0100

    Minor updates
    Minor updates

--- ex01/HelloWorld.java
+++ ex01/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld
 { // start declaration of the class HelloWorld
 	public static void main(String[] args)
     	{ // start declaration of main method
-	System.out.println("Hello World!"); // standard Hello World! greeting
-	} // end declaration of main method
+		System.out.println("Hello World!"); // standard Hello World! greeting
+		} // end declaration of main method
 } // end declaration of class HelloWorld
