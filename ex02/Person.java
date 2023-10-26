91a67c745e7e6b364e52d77fae17396e22fbf4c8
Armand979 <146201021+Armand979@users.noreply.github.com>
2023-10-26 13:05:31 +0100

    Update Person.java
    Changed name

--- ex02/Person.java
+++ ex02/Person.java
@@ -1,11 +1,11 @@
 /** 
-Mark Deegan <br>
+Armand Ediae <br>
 20231025 1639 <br>
 Person class <br>
 Sample code to demonstrate class declaration and inheritance (extending classes) <br>
 */
 public class Person
-{ // start declaration of public class Person
+{ // start declaration of public class Person 
 	public static void main(String[] args)
 	{ // start declaration of the main method
 		System.out.println("Hello, I am a Person!"); // print a simple message
