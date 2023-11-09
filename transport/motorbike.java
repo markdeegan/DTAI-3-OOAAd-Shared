3cf66842b9ab5ea56f48b1dfff4eb4427ca8cba2
ShaneDixon1 <146944056+ShaneDixon1@users.noreply.github.com>
2023-11-09 13:44:47 +0100

    Update motorbike.java

--- transport/motorbike.java
+++ transport/motorbike.java
@@ -1,6 +1,10 @@
 /** Shane Dixon, 9/11/2002, 12:44, Vehicle Project */
 public class motorbike extends landVehicle
   { 
+    public void setnumberofseats(int nOS)
+    {
+      numberofseats = nOS;
+    }
     private int numberoftyres;
     private int Strengthofengine;
     private int NeedofPPE;
