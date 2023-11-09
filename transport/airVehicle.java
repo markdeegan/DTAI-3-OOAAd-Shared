02e49561aa8ee419b57b004a38b8932b663ba009
Mark Deegan <mark.deegan@tudublin.ie>
2023-11-09 12:06:09 +0000

    Update and rename air-vehicle.java to airVehicle.java

--- transport/airVehicle.java
+++ transport/airVehicle.java
@@ -1,5 +1,5 @@
 /** paddy kelly 9.10.2023.11:44 vehicle*/
-public class air-vehicle extends vehicle
+public class airVehicle extends vehicle
 {
 }
 //*int number off engens  int number of tires  int number of propellers  int weaight of vehicle*/
