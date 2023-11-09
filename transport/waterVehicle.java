fb05748b4328d314c9689fbae8c94a6e4687ca62
Mark Deegan <mark.deegan@tudublin.ie>
2023-11-09 15:50:14 +0000

    Update waterVehicle.java

--- transport/waterVehicle.java
+++ transport/waterVehicle.java
@@ -1,7 +1,14 @@
-/** Shane Dixon, 9/11/2002, 12:45, Vehicle Project */
+////////////////////////////////////////
+/** 
+  Shane Dixon, 
+  9/11/2002, 12:45, 
+  Vehicle Project */
+////////////////////////////////////////
+
 public class waterVehicle extends vehicle
   { 
     private int Vehiclemadetobeusedinoceanoronwater;
     private int Usedforcruising,longtermcommuting,etc;
     private int extendstoboats,ships,jetskis,andrafts;
   }
+////////////////////////////////////////
