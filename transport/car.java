b3a7f7bd98bf66bc1ab499e07fc668019fc553ca
Armand979 <146201021+Armand979@users.noreply.github.com>
2023-11-09 13:45:25 +0000

    Update car.java
    Added in Comments

--- transport/car.java
+++ transport/car.java
@@ -2,14 +2,14 @@
 Armand Ediae <br>
 20231109 1149 <br>
 car class <br> */
-public class car extends landVehicle
+public class car extends landVehicle //Puting this in to extend the class land vehicle
   {
-    public void setnumberOfSeats(int nos)
+    public void setnumberOfSeats(int nos)// Setting the variable number of seats
     {
       numberOfSeats=nos;
     }
-    private int numberOfheadlights;
-    private int numberOfSeats;
+    private int numberOfheadlights;//Making the variable numberOfheadlights
+    private int numberOfSeats;//Making the variable numberOfseats
     // String Gearboxtype;
     
 }
