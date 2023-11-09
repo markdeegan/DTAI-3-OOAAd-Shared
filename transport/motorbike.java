503bb99edb475d8cb9ca3e7425bd2eabb5aae6cb
ShaneDixon1 <146944056+ShaneDixon1@users.noreply.github.com>
2023-11-09 13:21:08 +0100

    Update motorbike.java

--- transport/motorbike.java
+++ transport/motorbike.java
@@ -1,7 +1,11 @@
 /** Shane Dixon, 9/11/2002, 12:44, Vehicle Project */
 public class motorbike extends landVehicle
   { 
-  
+    private int numberoftyres;
+    private int Strengthofengine;
+    private int NeedofPPE;
+    private int stiffnessofhandlebars;
+    private int Numberofpeopletransported; 
 }
   
   
