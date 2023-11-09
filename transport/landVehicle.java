5c97b6e3861df679f1b5c4912d88ba4a665f2d3a
PaddykellyTUBlanch <146944139+PaddykellyTUBlanch@users.noreply.github.com>
2023-11-09 19:58:00 +0000

    Update landVehicle.java
    add setter methods, fixed spelling & added comments

--- transport/landVehicle.java
+++ transport/landVehicle.java
@@ -1,10 +1,39 @@
 /** paddy kelly 9.11.2023.11:44 vehicle*/
+// Description: This class represents a land vehicle and extends the vehicle class.
+
 public class landVehicle extends vehicle
+
 {
+// Declare private instance variables for landVehicle class
   private int numberOfTyres;
   private int amountOfCylindersInTheEngine;
   private int numberOfDoors;
   private int numberOfSeats;
-  private int siveOfFuleTank;
-  private int numberOfWings;
-}
+  private int sizeOfFuelTank;
+///  private int numberOfWings;
+// Setter method for setting the number of tyres
+  public void setNumberOfTyres(int nOT)
+  {
+    numberOfTyres = nOT;
+  }
+// Setter method for setting the amount of cylinders in the engine
+  public void setAmountOfCylinders(int cylinders) 
+  {
+    amountOfCylindersInTheEngine = cylinders;
+  }
+// Setter method for setting the number of doors
+  public void setNumberOfDoors(int doors) 
+  {
+    numberOfDoors = doors;
+  }
+// Setter method for setting the number of seats
+  public void setNumberOfSeats(int seats)
+  {
+    numberOfSeats = seats;
+  }
+// Setter method for setting the size of the fuel tank
+  public void setSizeOfFuelTank(int sOT)
+  {
+    sizeOfFuelTank = sOT;
+  }
+} // end declaration of class landVehicle
