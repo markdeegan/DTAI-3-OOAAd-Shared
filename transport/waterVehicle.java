96d8e47e5b7e038a68a97649f6d77bfadb702eab
ShaneDixon1 <146944056+ShaneDixon1@users.noreply.github.com>
2023-11-09 13:26:57 +0100

    Update waterVehicle.java

--- transport/waterVehicle.java
+++ transport/waterVehicle.java
@@ -1,4 +1,7 @@
 /** Shane Dixon, 9/11/2002, 12:45, Vehicle Project */
 public class waterVehicle extends vehicle
   { 
+    private int Vehiclemadetobeusedinoceanoronwater;
+    private int Usedforcruising,longtermcommuting,etc;
+    private int extendstoboats,ships,jetskis,andrafts;
   }
