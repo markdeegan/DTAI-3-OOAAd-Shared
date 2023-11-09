a34a0015fc08b99582dacda1c40bbe3b31d824e3
Armand979 <146201021+Armand979@users.noreply.github.com>
2023-11-09 13:55:44 +0000

    Update truck.java
    Added Comments
    Added Variables

--- transport/truck.java
+++ transport/truck.java
@@ -2,9 +2,14 @@
 Armand Ediae <br>
 20231109 1144<br>
 truck class <br> */
-public class truck extends landVehicle
-{
-  private int numberOfaxles;
-  private int numberOfexhaustpipes;
+public class truck extends landVehicle// Public class extends from the class landVehicle
+{ 
+  public void setnumberOfaxles(int nos)// Setting the variable numbeOfaxles
+    {
+      numberOfaxles=nos;
+    }
+  private int numberOfaxles;// Making the variable numberOfaxles
+  private int numberOfexhaustpipes;// Making the variable numberOfexhaust pipes
+  private int numberOfseats;// Making the variable numberOfSeats
   
 }
