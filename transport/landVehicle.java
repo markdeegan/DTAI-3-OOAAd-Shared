81d375fa699ef2f0b40367bf04daf9ae89fee141
Mark Deegan <mark.deegan@tudublin.ie>
2023-11-09 12:07:56 +0000

    Update landVehicle.java

--- transport/landVehicle.java
+++ transport/landVehicle.java
@@ -1,7 +1,7 @@
 /** paddy kelly 9.10.2023.11:44 vehicle*/
 public class landVehicle extends vehicle
 {
+  int numberOfTyres;
+  int amountOfCylindersInTheEngine;
+  int numberOfDoors;
 }
-int number of tiers
-int amount of cyillenders inn the engen
-int number of doors inclosed or exposed driver
