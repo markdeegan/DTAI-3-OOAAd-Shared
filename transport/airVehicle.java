cd0e65acd12d09480d7177006a89214419ceab3a
PaddykellyTUBlanch <146944139+PaddykellyTUBlanch@users.noreply.github.com>
2023-11-09 12:20:15 +0000

    Update airVehicle.java

--- transport/airVehicle.java
+++ transport/airVehicle.java
@@ -1,7 +1,7 @@
 /** paddy kelly 9.11.2023.11:44 vehicle*/
 public class airVehicle extends vehicle
 {
-  private int number off engens  
+  private int number of engens  
   private int number of tires  
   private int number of propellers  
   private int weaight of vehicle
