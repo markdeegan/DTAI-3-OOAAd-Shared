63303b854251e82d0ad852c0985ac80322ef3a47
Armand979 <146201021+Armand979@users.noreply.github.com>
2023-11-10 13:19:07 +0000

    Update car.java
    Modified code

--- transport/car.java
+++ transport/car.java
@@ -1,16 +1,34 @@
 /**
-Armand Ediae <br>
-20231109 1149 <br>
-car class <br> */
-public class car extends landVehicle //Puting this in to extend the class landvehicle
-  {
-    public void setnumberOfSeats(int nos)// Setting the variable numberOfseats
-    {
-      numberOfSeats=nos;
+ * Armand Ediae
+ * 20231109 1149
+ * Car class
+ */
+public class Car extends LandVehicle {
+
+    private int numberOfHeadlights;
+    private int numberOfSeats;
+    private float engineSize;
+    private String gearboxType;
+    private String fuelType;
+    private boolean isConvertible;
+    private String color;
+
+    // Constructor for initializing variables
+    public Car(int headlights, int seats, float size, String gearbox, String fuel, boolean convertible, String carColor) {
+        numberOfHeadlights = headlights;
+        numberOfSeats = seats;
+        engineSize = size;
+        gearboxType = gearbox;
+        fuelType = fuel;
+        isConvertible = convertible;
+        color = carColor;
     }
-    private int numberOfheadlights;//Making the variable numberOfheadlights
-    private int numberOfSeats;//Making the variable numberOfseats
-    Float Enginesize;// Making the Variable Enginesize
-    // String Gearboxtype;
+
+    // Setter method for the number of seats
+    public void setNumberOfSeats(int seats) {
+        numberOfSeats = seats;
+    }
+
     
 }
+
