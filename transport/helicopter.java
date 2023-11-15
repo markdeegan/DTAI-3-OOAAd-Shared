17baf1ec31cc103eaaa01108fab77840be8b9731
eoinfinn <B00143526@mytudublin.ie>
2023-11-15 10:33:39 +0000

    Create helicopter.java
    Creating helicopter class

---
+++ transport/helicopter.java
@@ -0,0 +1,7 @@
+/** Eoin Finnerty <br>
+Wednesday 20231115 1025 <br>
+This is to create class helicopter which extends class airVehicle
+*/
+
+//Declare the public class helicopter, extending air vehicle class
+public class helicopter extends airVehicle {}
