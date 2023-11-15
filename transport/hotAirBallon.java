ad74a1bf8260c0ae922b77d07be6114b73daafa9
eoinfinn <B00143526@mytudublin.ie>
2023-11-15 10:35:44 +0000

    Create hotAirBallon.java

---
+++ transport/hotAirBallon.java
@@ -0,0 +1,7 @@
+/** Eoin Finnerty <br>
+Wednesday 20231115 1034 <br>
+This is to create class hotAirBallon which extends class airVehicle
+*/
+
+//Declare the public class hotAirBallon, extending air vehicle class
+public class hotAirBallon extends airVehicle {}
