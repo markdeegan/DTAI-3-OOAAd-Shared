3e9d5083d0a9955999a170f3bb5ea9e5d7aa4a8b
niniolaman1 <147060572+niniolaman1@users.noreply.github.com>
2023-11-15 21:02:51 +0000

    Update ship.java

---
+++ transport/ship.java
@@ -0,0 +1,15 @@
+//Mansur Niniola
+//2023.15.11.14:53
+//ship class
+
+//declare the public class ship extends from the class watervehicle
+  public class ship extends waterVehicle{
+    private String name; //name of the ship
+    private float length; //length of the ship in meters
+    private float beam; //width of the ship at its widest point
+    private float weight; //the ship's weight
+    private int crewSize; //number of crew members
+    private int passengerCapacity;//how many passangers it can carry
+    private float maxSpeed;//maximum speed of the ships
+  
+}
