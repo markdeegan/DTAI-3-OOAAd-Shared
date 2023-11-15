eb9625d013480b2dae9ff44a0d4917cc50cf386d
eoinfinn <B00143526@mytudublin.ie>
2023-11-15 14:22:46 +0000

    Update helicopter.java

--- transport/helicopter.java
+++ transport/helicopter.java
@@ -6,12 +6,28 @@ This is to create class helicopter which extends class airVehicle
 //Declare the public class helicopter, extending air vehicle class
 public class helicopter extends airVehicle {
 
-	 //Variables
+	 	//Variables
 	    private int numberOfRotarySystems; //Total number of rotary systems needed to achieve flight
-	    private int numberOfMainRotarySystems; //Total number of main rotary systems
-	    private int numberOfRearRotarySystems; //Total number of rear rotary systems
+	    private int numberOfMainSystems; //Total number of main rotary systems
+	    private int numberOfRearSystems; //Total number of rear rotary systems
 	    private int numberOfBlades; //Total number of blades used by the helicopter
-	    private float mainRotaryBladeLength; //Length of blades on main rotary system(s)
-	    private float rearRotaryBladeLength; //Length of blades on rear rotary system(s)
+	    private float mainBladeLength; //Length of blades on main rotary system(s)
+	    private float rearBladeLength; //Length of blades on rear rotary system(s)
+
+		//Constructor with some variables
+		public helicopter(int rotary, int mains, int rears, int blades, float mainLength, float rearLength) {
+			     super();
+			     numberOfRotarySystems = rotary;
+			     numberOfMainSystems = mains;
+			     numberOfRearSystems = rears;
+			     numberOfBlades = blades;
+			     mainBladeLength = mainLength;
+			     rearBladeLength = rearLength;
+		}
+
+		//Default Constructor
+		public helicopter() {
+				super();
+		}
 
 	}
