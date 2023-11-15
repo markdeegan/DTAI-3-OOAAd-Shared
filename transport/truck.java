97fe5e3c4d1b50cdea8646dfe4abd7557ef39cdd
Armand979 <146201021+Armand979@users.noreply.github.com>
2023-11-15 13:03:36 +0000

    Update truck.java
    Modified Code

--- transport/truck.java
+++ transport/truck.java
@@ -1,53 +1,39 @@
-
-    /**
+ /**
  * Armand Ediae
- * 20231115 1029
+ * 20231115 1301
  * truck class
  */
 public class truck extends landVehicle {
 
-    // Private variables
-    private int numberOfAxles; // Number of axles
+    // Private variable specific to the truck class
     private int numberOfExhaustPipes; // Number of exhaust pipes
-    private int numberOfSeats; // Number of seats
     private float cargoCapacity; // Cargo capacity in cubic meters
     private String fuelType; // Type of fuel the truck uses
 
     // Full constructor for initializing all variables
     public truck(int axles, int exhaustPipes, int seats, float cargoCapacity, String fuelType) {
-        super(); // Assuming a default constructor in landVehicle, you can adjust this based on your actual implementation
-        numberOfAxles = axles;
+        super(axles, seats); // Assuming a constructor in landVehicle that accepts axles and seats
         numberOfExhaustPipes = exhaustPipes;
-        numberOfSeats = seats;
         this.cargoCapacity = cargoCapacity;
         this.fuelType = fuelType;
     }
 
     // Constructor with some parameters 
     public truck(int axles, int seats, String fuelType) {
-        super(); // Assuming a default constructor in landVehicle, you can adjust this based on your actual implementation
-        numberOfAxles = axles;
-        numberOfSeats = seats;
+        super(axles, seats); // Assuming a constructor in landVehicle that accepts axles and seats
         this.fuelType = fuelType;
     }
 
     // Default constructor with no parameters
     public truck() {
-        super(); // Assuming a default constructor in landVehicle, you can adjust this based on your actual implementation
+        super(); // Assuming a default constructor in landVehicle
         // Initialize default values or leave them as they are
     }
 
     // Getter methods for retrieving values
-    public int getNumberOfAxles() {
-        return numberOfAxles;
-    }
 
     public int getNumberOfExhaustPipes() {
         return numberOfExhaustPipes;
-    }
-
-    public int getNumberOfSeats() {
-        return numberOfSeats;
     }
 
     public float getCargoCapacity() {
@@ -60,19 +46,9 @@ public class truck extends landVehicle {
 
     // Setter methods for modifying values
 
-    // Setter for numberOfAxles
-    public void setNumberOfAxles(int axles) {
-        numberOfAxles = axles;
-    }
-
     // Setter for numberOfExhaustPipes
     public void setNumberOfExhaustPipes(int exhaustPipes) {
         numberOfExhaustPipes = exhaustPipes;
-    }
-
-    // Setter for numberOfSeats
-    public void setNumberOfSeats(int seats) {
-        numberOfSeats = seats;
     }
 
     // Setter for cargoCapacity
