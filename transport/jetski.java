5398dc9363051ddd523428632183d4e870a7f63e
ShalomOlorunfemi <146944313+ShalomOlorunfemi@users.noreply.github.com>
2023-11-15 10:17:52 +0000

    Update jetski.java

---
+++ transport/jetski.java
@@ -0,0 +1,15 @@
+   /**
+ * Shalom Olorunfemi
+ * 20231411
+ * jetski class
+ */
+public class jetski extends waterVehicle {
+   
+   // Private variables
+   private int numberOfTubes; // Number of tubes
+   private int numberOfSeams; // Number of seams
+   private int numberOfValves; // Number of valves
+   private int numberOfFloors; // Number of floors
+   private int numberOfThwarts; // Number of thwarts
+   private float cargoCapacity; // Cargo capacity in cubic meters
+   private String fuelType; // Type of fuel the truck uses
