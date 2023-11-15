494c0042c3b290c7672d296a50f0e1cbede8051e
niniolaman1 <147060572+niniolaman1@users.noreply.github.com>
2023-11-15 13:20:24 +0000

    Update plane.java

--- transport/plane.java
+++ transport/plane.java
@@ -5,16 +5,26 @@
 public class plane extends airVehicle {
   
   private int seat_number;
-  private string fuelType;
-  private int maxAltitude; // Maximum flying altitude
-  private int range; // Maximum range the plane can travel
-  private int wingSpan; // The wingspan of the plane
-  private int cargoCapacity; // Maximum cargo capacity
+  private int firstClassSeats; // Number of first-class seats
+  private int economyClassSeats; // Number of economy-class seats
+  private float maxAltitude; // Maximum flying altitude
+  private float range; // Maximum range the plane can travel
+  private float wingSpan; // The wingspan of the plane
+  private float cargoCapacity; // Maximum cargo capacity
   private String airline; // Associated airline
 
-    public plane( int seat_number, String fuelType, int maxAltitude, int range, int wingSpan, int cargoCapacity, String airline] ) {
-        super();
-        this.seatNumber = seatNumber;
-        this.setFuelType(fuelType); 
-    }
+  // basic constructor to set the most essential attributes of a plane
+   public Plane(int seat_number, float maxAltitude, String airline) {
+    this.seat_number = seat_number;
+    this.maxAltitude = maxAltitude;
+    this.airline = airline;
+}
+  // constructor with class specific seats
+  // for setting up planes with specific numbers of first-class and economy-class seats.
+  public Plane(int firstClassSeats, int economyClassSeats, String airline) {
+      this.firstClassSeats = firstClassSeats;
+      this.economyClassSeats = economyClassSeats;
+      this.airline = airline;
+}
+
 }
