7deffd31d201e7760385c387ba86053f0ff2b185
niniolaman1 <147060572+niniolaman1@users.noreply.github.com>
2023-11-15 11:43:17 +0000

    Update plane.java

--- transport/plane.java
+++ transport/plane.java
@@ -5,20 +5,16 @@
 public class plane extends airVehicle {
   
   private int seat_number;
-  private int numberOfEngines;
-  private int numberOfTires;
   private string fuelType;
-  private double maxAltitude; // Maximum flying altitude
-  private double range; // Maximum range the plane can travel
-  private double wingSpan; // The wingspan of the plane
-  private double cargoCapacity; // Maximum cargo capacity
+  private int maxAltitude; // Maximum flying altitude
+  private int range; // Maximum range the plane can travel
+  private int wingSpan; // The wingspan of the plane
+  private int cargoCapacity; // Maximum cargo capacity
   private String airline; // Associated airline
 
-    public plane(int numberOfEngines, int seat_number, int numberOfTires, String fuelType) {
+    public plane( int seat_number, String fuelType, int maxAltitude, int range, int wingSpan, int cargoCapacity, String airline] ) {
         super();
-        this.numberOfEngines = numberOfEngines;
         this.seatNumber = seatNumber;
-        this.numberOfTires = numberOfTires;
         this.setFuelType(fuelType); 
     }
 }
