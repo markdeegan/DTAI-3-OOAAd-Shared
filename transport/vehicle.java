0eaa869fe22e74f9f64f2e246e3354ad7376088c
ShaneDixon1 <146944056+ShaneDixon1@users.noreply.github.com>
2023-11-15 12:40:10 +0100

    Update vehicle.java

--- transport/vehicle.java
+++ transport/vehicle.java
@@ -1,7 +1,6 @@
 /** Shane Dixon, 15/11/2002, 12:44, Vehicle Project */
 public class vehicle
   {
-    private int Extendstoanamountofland,water,andairvehicles;
     private int Standsformachinecapableoftransportingpeople;
     private int Passengerspacevariesdependingonvehicle;
   }
