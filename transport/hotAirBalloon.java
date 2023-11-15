07cece6c851a47533d207825e4115873eb66dd2b
eoinfinn <B00143526@mytudublin.ie>
2023-11-15 11:01:21 +0000

    Change name of hotAirBallon.java to hotAirBalloon.java

---
+++ transport/hotAirBalloon.java
@@ -0,0 +1,13 @@
+/** Eoin Finnerty <br>
+Wednesday 20231115 1034 <br>
+This is to create class hotAirBallon which extends class airVehicle
+*/
+
+//Declare the public class hotAirBallon, extending air vehicle class
+public class hotAirBallon extends airVehicle {
+
+	 // Private variables
+	    private float balloonVolume //Define the maximum volume of the ballon
+	    private float basketWeight //Define the weight of the basket
+
+}
