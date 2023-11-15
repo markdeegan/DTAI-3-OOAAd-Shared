9cc63837dc9c5526608997f146ce4523fd8d3db6
eoinfinn <B00143526@mytudublin.ie>
2023-11-15 14:15:29 +0000

    Update hotAirBalloon.java

--- transport/hotAirBalloon.java
+++ transport/hotAirBalloon.java
@@ -6,12 +6,25 @@ This is to create class hotAirBallon which extends class airVehicle
 //Declare the public class hotAirBallon, extending air vehicle class
 public class hotAirBallon extends airVehicle {
 
-	 //Private variables
+	 	//Private variables
 	    private float balloonVolume; //Define the maximum volume of the balloon
 	    private float basketWeight; //Define the weight of the basket
-	    private float balloonVolume; //Define the weight of the balloon
+	    private float balloonWeight; //Define the weight of the balloon
 	    private int totalBalloonColours; //Total number of colours on the balloon
-	    //private List<String> colours; //List the colour(s) on the balloon
+
+	    //Constructor with some variables
+	    public hotAirBalloon(float volume, float basket, float balloon, int colours) {
+		        super();
+		        balloonVolume = volume;
+		        basketWeight = basket;
+		        balloonWeight = balloon;
+		        totalBalloonColours = colours;
+		}
+
+		//Default Constructor
+		public hotAirBalloon() {
+			super();
+		}
 
 
 }
