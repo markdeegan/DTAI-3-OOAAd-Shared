ccff7d76128add3f98b91ec833115abe7bfe3838
Armand979 <146201021+Armand979@users.noreply.github.com>
2023-11-30 13:25:57 +0000

    Update truck.java

--- transport/truck.java
+++ transport/truck.java
@@ -1,6 +1,6 @@
 /**
-* This class represents a truck, extending the functionality of a land vehicle.
-*
+This class represents a truck, extending the functionality of a land vehicle.
+
 @author Armand Ediae
 @version 20231115 1301
 @see car
@@ -14,38 +14,34 @@ public class truck extends landVehicle {
     // Private variables specific to the truck class
     private int numberOfExhaustPipes; // Number of exhaust pipes
     private float cargoCapacity; // Cargo capacity in cubic meters
-    private String fuelType; // Type of fuel the truck uses
+  
 
     /**
-     * Constructs a truck with specified parameters.
-     *
-     * @param axles Number of axles
-     * @param exhaustPipes Number of exhaust pipes
-     * @param seats Number of seats
-     * @param cargoCapacity Cargo capacity in cubic meters
-     * @param fuelType Type of fuel the truck uses
+     Constructs a truck with specified parameters.
+     
+     @param axles Number of axles
+     @param exhaustPipes Number of exhaust pipes
+     @param seats Number of seats
+      @param cargoCapacity Cargo capacity in cubic meters
+      @param fuelType Type of fuel the truck uses
      */
     public truck(int axles, int exhaustPipes, int seats, float cargoCapacity, String fuelType) {
-        super(axles, seats);
+        super(axles, seats, fuelType);
         numberOfExhaustPipes = exhaustPipes;
         this.cargoCapacity = cargoCapacity;
-        this.fuelType = fuelType;
+        
     }
 
     /**
-     * Constructs a truck with specified parameters.
-     *
-     * @param axles Number of axles
-     * @param seats Number of seats
-     * @param fuelType Type of fuel the truck uses
+      Constructs a truck with specified parameters.
+     
+      @param axles Number of axles
+      @param seats Number of seats
+      @param fuelType Type of fuel the truck uses
      */
-    public truck(int axles, int seats, String fuelType) {
-        super(axles, seats);
-        this.fuelType = fuelType;
-    }
-
+   
     /**
-     * Default constructor with no parameters.
+      Default constructor with no parameters.
      */
     public truck() {
         super();
