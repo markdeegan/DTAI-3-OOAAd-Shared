1f896cfc1f4c73495e8a9f35422982c0330c5f46
Mark Deegan <1252943+markdeegan@users.noreply.github.com>
2023-11-30 10:51:06 +0000

    :Adding asset interface

---
+++ transport/asset.java
@@ -0,0 +1,11 @@
+/** Mark Deegan
+Thu 30 Nov 2023 10:45:50 GMT
+interface to make things into assets
+*/
+
+public interface asset
+{
+	float value =0.0f;
+	public float getValue();
+	public void setValue(float assetValue);
+}
