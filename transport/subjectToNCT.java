a00992628c3b83efbcd0ea796ac6d3a4f92c39ac
Mark Deegan <mark.deegan@tudublin.ie>
2023-12-01 10:50:21 +0000

    Update subjectToNCT.java
    Adding javadoc comments to interface subjectToNCT

--- transport/subjectToNCT.java
+++ transport/subjectToNCT.java
@@ -1,2 +1,11 @@
-public  interface subjectToNCT {
+/** Mark Deegan
+@author <a href = "mailto: mark.deegan@tudublin.ie">Mark Deegan</a><br>
+Thu 30 Nov 2023 10:45:50 GMT<br>
+subjectToNCT interface to make things be subjectToNCT
+*/
+
+public interface subjectToNCT
+{
+  /** there are no methods or data members for this interface<br>
+  that means that implementing classes do not need to complete any missing methods etc. */
 }
