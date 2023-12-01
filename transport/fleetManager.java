2401b59cb12759cb05e643216aec40099b919079
Mark Deegan <mark.deegan@tudublin.ie>
2023-12-01 13:41:59 +0000

    Updating fleetManager

--- transport/fleetManager.java
+++ transport/fleetManager.java
@@ -11,43 +11,39 @@ public class fleetManager
 	/** main method thjat will test the creation and management of some vehicles */
 	public static void main(java.lang.String[] args)
 	{ // begin declaration of the main method
-		waitASecond();
+		waitASecond(1);
 		clearScreen();
-		waitASecond();
+		waitASecond(1);
 		System.out.println("Hello. I am the fleet manager");
 		System.out.println("I am going to create some instances of vehicles.");
 		System.out.println("These will all be instances of classes derived from the superclass (base class) vehicle");
 
-		waitASecond();
-		waitASecond();
+		waitASecond(2);
 		System.out.println("Let's wait a little, and pretend this takes a while to complete...");
-		waitASecond();
-		waitASecond();
+		waitASecond(2);
 		System.out.println("processing your order...");
-		waitASecond();
-		waitASecond();
+		waitASecond(2);
 		System.out.println("Done!");
-		waitASecond();
-		waitASecond();
+		waitASecond(2);
 
 
-		car myCar 		= new car();		// create an anonymous car (no parameters)
 		truck myTruck 		= new truck();		// create an anonymous truck (no parameters)
 		motorbike myMotorbike 	= new motorbike();	// create an anonymous motorbike (no parameters)
+		car myCar 		= new car();		// create an anonymous car (no parameters)
 		plane myPlane		= new plane(200, 16, "Aer Lingus"); // create a plane with 200 economy and 16 first-class seats
 		hotAirBalloon myHAB	= new hotAirBalloon();	// create an anonymous hotAirBalloon (no parameters)
 		helicopter myHelicopter	= new helicopter();	// create an anonymous helicopter (no parameters)
 		
 		System.out.println("Now that we have created them, we will print them out");
-			waitASecond();
-		System.out.println(myCar);
-			waitASecond();
+			waitASecond(1);
 		System.out.println(myTruck);
-			waitASecond();
+			waitASecond(1);
 		System.out.println(myMotorbike);
-			waitASecond();
+			waitASecond(1);
+		System.out.println(myCar);
+			waitASecond(1);
 		System.out.println(myPlane);
-			waitASecond();
+			waitASecond(1);
 
 		System.out.println();
 		System.out.println("All done!");
@@ -63,11 +59,13 @@ public class fleetManager
 		System.out.flush();  
 	}
 
-	public static void waitASecond()
+	/** waitASecond method to wait a fixed number of seconds
+	default value, 1 second, otherwise seconds*1000 milliseconds */
+	public static void waitASecond(int seconds)
 	{ // start waitASecond method
 		try 
 		{ // start try block
-            		Thread.sleep(1000);
+            		Thread.sleep(seconds * 1000);
         	} // end try block
 		catch (InterruptedException e) 
 		{ // start catch block
