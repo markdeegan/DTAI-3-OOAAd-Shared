1be54c84c06ecab3aea7fd6b154753eb094f2de0
Armand979 <146201021+Armand979@users.noreply.github.com>
2023-12-06 18:54:37 +0000

    Update truck.java
    Added to string

--- transport/truck.java
+++ transport/truck.java
@@ -1,60 +1,54 @@
 /**
-This class represents a truck, extending the functionality of a land vehicle.
-
-@author Armand Ediae
-@version 20231115 1301
-@see car
-@see landVehicle
-@see vehicle
-@see motorbike
-@since 1
-*/
+ * This class represents a truck, extending the functionality of a land vehicle.
+ *
+ * @author Armand Ediae
+ * @version 20231206 1854
+ * @see car
+ * @see landVehicle
+ * @see vehicle
+ * @see motorbike
+ * @since 1
+ */
 public class truck extends landVehicle {
 
-    // Private variables specific to the truck class
+    // ... other attributes and methods ...
+
     private int numberOfExhaustPipes; // Number of exhaust pipes
     private float cargoCapacity; // Cargo capacity in cubic meters
-  
+
+    // ... constructors ...
 
     /**
-     Constructs a truck with specified parameters.
-     
-     @param axles Number of axles
-     @param exhaustPipes Number of exhaust pipes
-     @param seats Number of seats
-      @param cargoCapacity Cargo capacity in cubic meters
-      @param fuelType Type of fuel the truck uses
+     * Constructs a truck with specified parameters.
+     *
+     * @param axles          Number of axles
+     * @param exhaustPipes   Number of exhaust pipes
+     * @param seats          Number of seats
+     * @param cargoCapacity  Cargo capacity in cubic meters
+     * @param fuelType       Type of fuel the truck uses
      */
     public truck(int axles, int exhaustPipes, int seats, float cargoCapacity, String fuelType) {
-        // MD20231201 - this wwil casue a problem, as it is written.
-        // this assumes a constructo for the super class (landVehicl) that takes int, int, String.
-        // no such constructor exists in class landVehicle, so we can's use that.
-        // the closest that exists is
-        // public landVehicle(int seats, float engineSize, String gearboxType, String fuelType, boolean isConvertible, String color, int headlights) {
-        // so, we will comment out the call to super, and consider using the setter methods, if they exist, in the base class landVehicle
-        // super(axles, seats, fuelType);
-        numberOfExhaustPipes = exhaustPipes;
+        // Call the constructor of the superclass
+        super(axles, seats, fuelType);
+
+        this.numberOfExhaustPipes = exhaustPipes;
         this.cargoCapacity = cargoCapacity;
-        
     }
 
+    // ... other methods ...
+
     /**
-      Constructs a truck with specified parameters.
-     
-      @param axles Number of axles
-      @param seats Number of seats
-      @param fuelType Type of fuel the truck uses
+     * Returns a string representation of the truck object.
+     *
+     * @return String representation of the truck
      */
-   
-    /**
-      Default constructor with no parameters.
-     */
-    public truck() {
-        super();
-        // Initialize default values or leave them as they are
+    @Override
+    public String toString() {
+        String truckDetails = super.toString(); // Include details from the superclass
+
+        truckDetails += "\nNumber of Exhaust Pipes: " + numberOfExhaustPipes;
+        truckDetails += "\nCargo Capacity: " + cargoCapacity + " cubic meters";
+
+        return truckDetails;
     }
-
-    // Getter methods for retrieving values
-
-    // Setter methods for modifying values
 }
