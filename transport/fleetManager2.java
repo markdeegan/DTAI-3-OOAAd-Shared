61c2deb1380930b09283a7c044006e3e650e93f2
Mark Deegan <mark.deegan@tudublin.ie>
2023-12-07 10:47:27 +0000

    Making changes to fleetManager2 to reflect polymorphism

--- transport/fleetManager2.java
+++ transport/fleetManager2.java
@@ -1,14 +1,12 @@
+import java.util.ArrayList;
+
 /**
 Application to test the creation and management of a fleet of vehicles<br>
 class fleet manager 2 will be used to create and manuipulate a number of objects all extended from the super class vehicle <br>
-
 @author Mark Deegan <a href="mailto:mark.deegan@tudublin.ie">Send Mail</a>
 @version 202311171036
 @see vehicle
 */
-
-import java.util.ArrayList;
-
 public class fleetManager2
 { // start declaration of the fleetManager2 class
 
@@ -16,29 +14,75 @@ public class fleetManager2
 	public static void main(java.lang.String[] args)
 	{ // begin declaration of the main method
 
+	/** declare an ArrayList to store objects of class vehicle etc. */
 	ArrayList<vehicle> myFleet = new ArrayList<vehicle>();	
-		waitASecond(1);
-		clearScreen();
+
+	/** declare an ArrayList to store objects of class car etc. */
+	ArrayList<car> myCars = new ArrayList<car>();
+
+
+	clearScreen();
+	
+	System.out.println("Creating some instances of vehicles");
+	System.out.println();
+	waitASecond(1);
 
 		truck myTruck 		= new truck();		// create an anonymous truck (no parameters)
-			myFleet.add(myTruck);
+			myFleet.add(myTruck);			// add the truck to the fleet
 		motorbike myMotorbike 	= new motorbike();	// create an anonymous motorbike (no parameters)
-			myFleet.add(myMotorbike);
+			myFleet.add(myMotorbike);		// add the motorbike to the fleet
 		car myCar 		= new car();		// create an anonymous car (no parameters)
-			myFleet.add(myCar);
+			myFleet.add(myCar);			// add the car to the fleet
 		plane myPlane		= new plane(200, 16, "Aer Lingus"); // create a plane with 200 economy and 16 first-class seats
-			myFleet.add(myPlane);
+			myFleet.add(myPlane);			// add the plane to the fleet
 		hotAirBalloon myHAB	= new hotAirBalloon();	// create an anonymous hotAirBalloon (no parameters)
-			myFleet.add(myHAB);
+			myFleet.add(myHAB);			// add the Hot Air Balloon to the fleet
 		helicopter myHelicopter	= new helicopter();	// create an anonymous helicopter (no parameters)
-			myFleet.add(myHelicopter);
+			myFleet.add(myHelicopter);		// add the helicopter to the fleet
 		
 		System.out.println("Now that we have created them, we will print them out");
 			waitASecond(1);
 
+		System.out.println("Printing out all vehicles.");
+		// loop through, from i = 0 to i < myFleetSize 
     		for (int i = 0; i < myFleet.size(); i++) {
+			// just print out the object, relying on the toString method 
       			System.out.println(myFleet.get(i));
 		}
+		System.out.println();
+		waitASecond(1);
+		
+		System.out.println("Adding cars only to myCars container");
+    		for (int j = 0; j < myFleet.size(); j++) {
+			// check if the object is of class car,
+			// if it is, add it to the myCars array container
+			vehicle currentVehicle = myFleet.get(j);
+			if (currentVehicle instanceof car)
+			{
+				System.out.println(currentVehicle + " is an instance of class car");
+				System.out.println("So, let's add it to the myCars ArrayList.");
+				System.out.println();
+				myCars.add((car)currentVehicle);
+			}
+			else {
+	
+				System.out.println(currentVehicle + " is not an instance of class car");
+				System.out.println("So, we won't be adding it to the myCars arrayList.");
+				System.out.println();
+			}
+		}	
+		System.out.println();
+		waitASecond(1);
+		
+		System.out.println("Printing out only cars.");
+		// loop through, from i = 0 to i < myCarsSize 
+    		for (int k = 0; k < myCars.size(); k++) {
+			// just print out the object, relying on the toString method 
+			car currentCar = myCars.get(k);
+      			System.out.println(currentCar);
+		}
+		System.out.println(myFleet);
+		System.out.println(myCars);
 
 		System.out.println();
 		System.out.println("All done!");
