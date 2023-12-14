4ce6eef84fbdfdd6220de0d4ce9af9acccf56452
Mark Deegan <mark.deegan@tudublin.ie>
2023-12-14 12:35:28 +0000

    :removing reference to suprt from class car

--- transport/car.java
+++ transport/car.java
@@ -23,7 +23,7 @@ public class car extends landVehicle implements subjectToNCT {
      * @param color        Color of the car
      */
     public car(int headlights, int seats, float engineSize, String gearboxType, String fuelType, boolean isConvertible, String color) {
-        super(headlights, seats, fuelType, engineSize, gearboxType, color, isConvertible);
+        // super(headlights, seats, fuelType, engineSize, gearboxType, color, isConvertible);
     }
 
     public car(int headlights, int seats, float engineSize, boolean isConvertible) {
