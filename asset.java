29f9e468d91d290e84a3f86c245bc9fbf81faf2d
Armand979 <146201021+Armand979@users.noreply.github.com>
2023-12-24 22:13:59 +0000

    update asset java

--- asset.java
+++ asset.java
@@ -3,11 +3,3 @@ public interface asset {
     // Define methods or attributes here
 }
 
-// MainClass.java
-public class MainClass implements asset {
-    public static void main(String[] args) {
-        // Your main method code here
-    }
-
-    // Implement methods from the Asset interface here
-}
